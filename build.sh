e41db8a4435637e2894b02641a3d85dee07693c1
Hilda-Enyioko <enyiokohilda@gmail.com>
2026-07-02 19:40:39 +0100

    fix: update build.sh to collect drf static files

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
+pip install -r requirements.txt && python manage.py collectstatic --noinput
 DATABASE_URL="$DIRECT_DATABASE_URL" python manage.py migrate
