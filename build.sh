b05a78c415e369e9be4b9cad3406db2d4401334c
Hilda-Enyioko <enyiokohilda@gmail.com>
2026-07-02 20:04:34 +0100

    fix: update build command

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt && python manage.py collectstatic --noinput
+pip install -r requirements.txt && python manage.py collectstatic --noinput --ignore="*.map"
 DATABASE_URL="$DIRECT_DATABASE_URL" python manage.py migrate
