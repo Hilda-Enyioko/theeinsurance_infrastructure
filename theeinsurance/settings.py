69a009e6d533e9cce5346e5d74baffd9a6e45175
Hilda-Enyioko <enyiokohilda@gmail.com>
2026-07-07 00:46:06 +0100

    fix: remove unnecessary path from cors origin

--- theeinsurance/settings.py
+++ theeinsurance/settings.py
@@ -183,7 +183,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://mock-insurance-customer-portal.vercel.app",
-    "https://theeinsurance-portal.vercel.app/"
+    "https://theeinsurance-portal.vercel.app"
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
